e0d611e7f97db16c9f41fbf882d25fcbefb5e02d
kimkyungmin <kimkyungmin@sonline20.sen.go.kr>
2021-08-26 09:45:40 +0900

    wander

--- Assets/Scripts/MonoBehaviors/Wander.cs
+++ Assets/Scripts/MonoBehaviors/Wander.cs
@@ -6,7 +6,7 @@ public class Wander : MonoBehaviour
     public float pursuitSpeed;
     public float wanderSpeed;
     float currentSpeed;
-    float currentAngle;
+    //float currentAngle;
 
     public float directionChangeInterval;
 
@@ -16,11 +16,13 @@ public class Wander : MonoBehaviour
 
     CircleCollider2D circleCollider;
     Rigidbody2D rb2d;
+    Animator animator;
 
     private void Start()
     {
         circleCollider = GetComponent<CircleCollider2D>();
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentSpeed = wanderSpeed;
         StartCoroutine(wanderRoutine());
@@ -36,41 +38,67 @@ public class Wander : MonoBehaviour
 
     private void Update()
     {
-        Debug.DrawLine(rb2d.position, endPosition, Color.red);
+        Debug.DrawLine(transform.position, endPosition, Color.red);
     }
 
     private IEnumerator wanderRoutine()
     {
         while (true)
         {
-            //«“¿œ
             endPosition = ChooseNewEndposition();
 
             if (moveCoroutine != null)
             {
                 StopCoroutine(moveCoroutine);
             }
-            moveCoroutine = StartCoroutine(move());
+            moveCoroutine = StartCoroutine(Move());
             yield return new WaitForSeconds(directionChangeInterval);
         }
     }
 
-    private IEnumerator move()
+    private IEnumerator Move()
     {
-        Debug.Log("move....... start");
         while (true)
         {
-            //Debug.Log("move");
             yield return new WaitForFixedUpdate();
         }
     }
 
     private Vector3 ChooseNewEndposition()
     {
-        currentAngle += Random.Range(0, 360);
-        currentAngle = Mathf.Repeat(currentAngle, 360);
-        
+        float currentAngle = Random.Range(0, 360);
         float inputAngleRadians = currentAngle * Mathf.Deg2Rad;
         return new Vector3(Mathf.Cos(inputAngleRadians), Mathf.Sin(inputAngleRadians), 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            currentSpeed = pursuitSpeed;
+            animator.SetBool("isWalking", true);
+
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            moveCoroutine = StartCoroutine(Move());
+            targetTransform = collision.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            currentSpeed = wanderSpeed;
+            animator.SetBool("isWalking", false);
+
+            if (moveCoroutine != null)
+            {
+                StopCoroutine(moveCoroutine);
+            }
+            targetTransform = null;
+        }
     }
 }
